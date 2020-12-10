d265485587b95d329233f9028f49008c9d15ee08
Bram Ceulemans <bramceulemans@me.com>
2020-12-10 02:58:26 +0100

    Add deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,19 @@
 const pages = require('gh-pages')
 
+const ci = process.env.CI;
+
 const deploy = async (dir, config) => new Promise(resolve => pages.publish(dir, config, resolve))
 
 const run = async () => {
     let message = 'Development build'
-    if (process.env.CI) {
+    if (ci) {
         const commit = process.env.GITHUB_SHA.substr(0, 7);
         message = `Build for commit ${commit}`
     }
     await deploy('build', {
         branch: 'gh-pages',
         message: message,
-        push: false,
+        push: ci,
     })
 }
 
