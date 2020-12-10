4d049a3dd37b995be32d7882fd9a7ae315d7c3bc
Bram Ceulemans <bramceulemans@me.com>
2020-12-10 03:13:41 +0100

    Remove build

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,5 @@
-const pages = require('gh-pages')
-
-const deploy = async (dir, config) => new Promise(resolve => pages.publish(dir, config, resolve))
-
 const run = async () => {
-    let message = 'Development build'
-    if (process.env.CI) {
-        const commit = process.env.GITHUB_SHA.substr(0, 7);
-        message = `Build for commit ${commit}`
-    }
-    await deploy('build', {
-        branch: 'gh-pages',
-        message: message,
-        history: false,
-        push: true,
-    })
+    // build code
 }
 
 run()
